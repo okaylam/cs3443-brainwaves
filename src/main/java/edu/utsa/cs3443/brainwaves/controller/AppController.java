af1d839d6d1f40404ad3a9c2da4ee3d0d95609e0
okaylam <kaylajanae1219@gmail.com>
2025-11-13 20:27:31 -0600

    Made notes and focus buttons functional

--- src/main/java/edu/utsa/cs3443/brainwaves/controller/AppController.java
+++ src/main/java/edu/utsa/cs3443/brainwaves/controller/AppController.java
@@ -3,7 +3,6 @@ package edu.utsa.cs3443.brainwaves.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 
 import java.io.IOException;
@@ -11,7 +10,7 @@ import java.io.IOException;
 public class AppController {
     @FXML private StackPane contentHolder;
 
-    private enum View { HOME, TASKS }
+    private enum View { HOME, TASKS, NOTES, FOCUS }
 
     @FXML
     public void initialize() {
@@ -20,6 +19,8 @@ public class AppController {
 
     @FXML private void goHome() { show(View.HOME); }
     @FXML private void goTasks() { show(View.TASKS); }
+    @FXML private void goNotes() { show(View.NOTES); }
+    @FXML private void goFocus() { show(View.FOCUS); }
 
     private void show(View v) {
         Parent view = loadView(v);
@@ -29,7 +30,9 @@ public class AppController {
     private Parent loadView(View v) {
         String fxml = switch (v) {
             case View.HOME -> "/edu/utsa/cs3443/brainwaves/fxml/home-view.fxml";
-            case View.TASKS -> "/edu/utsa/cs3443/brainwaves/fxml/task-view-copy.fxml";
+            case View.TASKS -> "/edu/utsa/cs3443/brainwaves/fxml/task-view.fxml";
+            case View.NOTES -> "/edu/utsa/cs3443/brainwaves/fxml/notes-view.fxml";
+            case View.FOCUS -> "/edu/utsa/cs3443/brainwaves/fxml/focus-view.fxml";
         };
 
         try {
